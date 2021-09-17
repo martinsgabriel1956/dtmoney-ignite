5faf16aeef626eb154c2c93167b1267dfe186a31
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-17 14:30:28 -0300

    feat: installing styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`;
+
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Title>Hello World!</Title>
+      <button>Salvar</button>
+    </>
   );
 }
