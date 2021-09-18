986eea371f2c6a4a92e772da53b6c5e8dc2b0b40
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-18 16:48:37 -0300

    feat: setting modal creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { createServer } from 'miragejs';
+import { createServer } from "miragejs";
+import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
 
 createServer({
   routes() {
-    this.namespace = 'api';
+    this.namespace = "api";
 
-    this.get('/transactions', () => {
+    this.get("/transactions", () => {
       return [
         {
           id: 1,
-          title: 'Transition 1',
+          title: "Transition 1",
           amount: 400,
-          type: 'deposit',
-          category: 'food',
+          type: "deposit",
+          category: "food",
           date: new Date(),
-        }
-      ]
-    })
-  }
-})
+        },
+      ];
+    });
+  },
+});
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Usuário</h2>
+      </Modal>
     </>
   );
 }
