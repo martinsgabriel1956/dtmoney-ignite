40e41658cb5e0252e13780e27c6d71354f6e24e4
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-17 23:23:04 -0300

    feat: setting axios client

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
+
 import { Container } from './styles';
+
+import { api } from '../../services/api';
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions').then(res => res.json()).then(data => console.log(data));
+    api.get('transactions').then(res => console.log(res.data));
   }, [])
 
   return (
