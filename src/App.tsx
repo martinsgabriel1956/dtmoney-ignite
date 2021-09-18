49828ed94fca048e5a8f0971407789120826295b
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-17 22:57:50 -0300

    feat: setting miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { createServer } from 'miragejs';
+
 import { GlobalStyle } from "./styles/global";
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transition 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          date: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
